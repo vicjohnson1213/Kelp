43dc86d0006c5e2a99753830dc9ad3a07b8debec
Victor Johnson <vicjohnson1213@gmail.com>
2015-10-01 01:59:30 -0600

    Added more string functions.

--- test/functions/string.js
+++ test/functions/string.js
@@ -4,7 +4,7 @@ var expect = require('expect.js'),
 describe('string functions:', function() {
     describe('substring', function() {
         it('should return substring', function() {
-            expect(string.substring(['some string', 2, 5])).to.equal('me st');
+            expect(string.substring(['some string', 2, 5])).to.equal('me ');
         });
 
         it('should return substring to the end', function() {
@@ -14,6 +14,21 @@ describe('string functions:', function() {
         it('should return error for bad args', function() {
             expect(string.substring(['some string', 'f', undefined])).to.equal('Invalid arguments to (substring string number number)');
             expect(string.substring([3, 2, undefined])).to.equal('Invalid arguments to (substring string number number)');
+        });
+    });
+
+    describe('substr', function() {
+        it('should return substring', function() {
+            expect(string.substr(['some string', 2, 6])).to.equal('me str');
+        });
+
+        it('should return substring to the end', function() {
+            expect(string.substr(['some string', 2])).to.equal('me string');
+        });
+
+        it('should return error for bad args', function() {
+            expect(string.substr(['some string', 'f', undefined])).to.equal('Invalid arguments to (substr string number number)');
+            expect(string.substr([3, 2, undefined])).to.equal('Invalid arguments to (substr string number number)');
         });
     });
 
@@ -28,6 +43,27 @@ describe('string functions:', function() {
 
         it('should return error for bad args', function() {
             expect(string.append(['some string', 5])).to.equal('Invalid arguments to (append string ...)');
+        });
+    });
+
+    describe('trim', function() {
+        it('should trim a string', function() {
+            expect(string.trim(['   some string   '])).to.equal('some string');
+        });
+
+        it('should return error for bad args', function() {
+            expect(string.trim([5])).to.equal('Invalid arguments to (trim string)');
+        });
+    });
+
+    describe('indexOf', function() {
+        it('should find index of a string in a string', function() {
+            expect(string.indexOf(['some string', 'string'])).to.equal(5);
+        });
+
+        it('should return error for bad args', function() {
+            expect(string.indexOf([5, 'asdf'])).to.equal('Invalid arguments to (indexOf string string)');
+            expect(string.indexOf(['asdf', 5])).to.equal('Invalid arguments to (indexOf string string)');
         });
     });
 });
